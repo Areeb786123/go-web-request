ad7e36f0ca680ae3b1aa7f94f162f6b5304d72d6
areeb786 <areebrockzz786@gmail.com>
2023-12-25 02:22:54 +0530

    added some data

--- buildApi/main.go
+++ buildApi/main.go
@@ -1,11 +1,20 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
 // model for courses
 type Course struct {
 	CourseId    string  `json:"courseId"`
 	CourseName  string  `json:"courseName"`
 	CoursePrice string  `json:"coursePrice"`
-	Author      *Author `json:"author"` // using ` ` because it works as @Serialzed name as we do it  in  android
+	Author      *Author `json:"author"`
+
+	/** using ` ` because it works as
+	@Serialzed name as we do it  in  android **/
 
 }
 
@@ -14,8 +23,19 @@ type Author struct {
 }
 
 // Fake DB
-var course []Course
+var courses []Course
 
 func main() {
 
+}
+
+// controllers
+func homeController(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Welcome to home controller</h1>"))
+}
+
+func getAllCourses(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get all courses")
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(courses)
 }
